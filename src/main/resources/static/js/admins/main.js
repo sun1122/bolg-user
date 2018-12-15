b41f1f8d73c50fc931eb3e0f0d078633b18a12d4
周家申 <282581063@qq.com>
2018-12-15 22:52:20 +0800

    后台管理主页的js编写

--- src/main/resources/static/js/admins/main.js
+++ src/main/resources/static/js/admins/main.js
@@ -6,12 +6,30 @@
 //# sourceURL=main.js
 
 // DOM 加载完再执行
+//$(function(){ }是$(document).ready(function()的简写，相当于window.onload = function(){ } ,
+//虽然这段jquery代码与javascript代码在功能上可以互换，但执行的时间不一样，
+//前者页面框架加载完成就执行，后者页面中所有内容加载完成才执行
 $(function() {
 
-	// 搜索
+	// 菜单事件
 	$(".menu .list-group-item").click(function() {
-		console($(this).value);
+		var url = $(this).attr("url");//获取地址url
+		// 先移除其他的点击样式，再添加当前的点击样式
+		$(".blog-menu .list-group-item").removeClass("active");
+		$(this).addClass("active");
+		
+		//加载其他模块的页面 到右侧区域
+		$.ajax({
+			url: url,
+			success: function(data){
+				$("#rightContainer").html(data);
+			},
+			error: function(){
+				alert("error");
+			}
+		});
 	});
-	
+	// 选中菜单第一项
+    $(".blog-menu .list-group-item:first").trigger("click");
 
 });
