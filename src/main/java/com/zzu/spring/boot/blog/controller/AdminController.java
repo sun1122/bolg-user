e56b4356ed015ee9faf8e929aebba5d9295a8ca1
周家申 <282581063@qq.com>
2018-12-15 22:54:48 +0800

    后台控制器

--- src/main/java/com/zzu/spring/boot/blog/controller/AdminController.java
+++ src/main/java/com/zzu/spring/boot/blog/controller/AdminController.java
@@ -5,11 +5,16 @@
  */
 package com.zzu.spring.boot.blog.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.zzu.spring.boot.blog.vo.Menu;
 
 /**
  * @ClassName: AdminController
@@ -22,8 +27,21 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admins")
 public class AdminController {
  
+	/**
+	 * 
+	 * @Title: listUsers  
+	 * @Description: TODO(动态获取菜单)  
+	 * @param @param model
+	 * @param @return    参数  
+	 * @return ModelAndView    返回类型  
+	 * @throws
+	 */
 	@GetMapping
-	public ModelAndView listUsers(Model model) {
-		return new ModelAndView("admins/index", "menuList", model);
-	}
+    public ModelAndView listUsers(Model model) {
+		//可以从数据库加载，目前先在内存中测试，后期改造
+        List<Menu> list = new ArrayList<>();
+        list.add(new Menu("用户管理", "/users"));
+        model.addAttribute("list", list);
+        return new ModelAndView("/admins/index", "model", model);
+    }
 }
